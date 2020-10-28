3b7c9999217dfaa9e7cc9940c2d2aa6bde70d753
bastian caceres <bast.caceresr@gmail.com>
2020-10-28 16:02:32 -0300

    primera parte

---
+++ ejercicio2/assets/js/script.js
@@ -0,0 +1,10 @@
+var num1 = parseFloat(prompt("ingrese el primer numero: ")) > 0; // parseFloat numero decimal-parseInt numero entero
+var num2 = parseFloat(prompt("ingrese el segundo numero: ")) > 0;
+
+var sum = num1 + num2;
+var res = num1 - num2;
+var div = num1 / num1;
+var multi = num1 * num2;
+var modulo = num1 % num2;
+
+alert("la suma de " + num1 +" + "+ num2 + " es: "+ sum );
